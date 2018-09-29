1fa64c4a16e389a94d196d3afaeb066c7cfdc5f9
f4r4zas <you@example.com>
2018-09-30 03:46:28 +0500

    pricing update 30/9/2018

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -3,5 +3,40 @@
 
 @include("includes.full_header")
 </span>
+<style type="text/css">
+
+	.form-horizontal{
+		padding:100px;	
+	}
+
+	
+</style>
+
+<div class="container">
+	<p class="clearfix"></p>
+	<div class="col-md-12">
+
+
+		<form class="form-horizontal">
+
+			<h2>Fare Estimate</h2>
+
+			<br>
+			<br>
+
+			<div class="col-md-4">
+				<input type="text" placeholder="Enter pick-up Location" class="form-control">
+			</div>
+			<div class="col-md-4">
+				<input type="text" placeholder="Enter drop-off Location" class="form-control">
+			</div>
+			<div class="col-md-4">
+				<input type="button" class="form-control btn-primary" value="Get Estimate">
+			</div>
+		
+		</form>
+
+	</div><!-- Form -->
+</div>
 
 @include('includes.footer')
