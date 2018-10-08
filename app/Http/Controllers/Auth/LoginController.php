712bd263ce7379848ddb33eeaeb5f0092c7ebc8d
Irfan Mumtaz <im.irfanmumtaz21@gmail.com>
2018-10-08 10:40:20 +0500

    reset password email work

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,4 +41,43 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function passwordReset(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if(!is_null($user)){
+            $token = app('auth.password.broker')->createToken($user);
+
+            $reset = DB::table('password_resets')->insert([
+                'email' => "irfan@nextgeni.com",
+                'token' => $token,
+                'created_at' => Carbon::now(),
+            ]);
+            $url = env("APP_URL", "http://localhost/") . "/password/reset/" . $token; 
+            $this->sendPaswordResetEmail($user, $url);
+            return redirect()->back();
+        }
+        dd("No user found");
+    }
+
+    private function sendPaswordResetEmail($user, $url){
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP(); // tell to use smtp
+            $mail->CharSet = "utf-8"; // set charset to utf8
+            $mail->SMTPAuth = true;  // use smpt auth
+            $mail->SMTPSecure = "tls"; // or ssl
+            $mail->Host = "smtp.gmail.com";
+            $mail->Port = 587; // most likely something different for you. This is the mailtrap.io port i use for testing. 
+            $mail->Username = env("MAIL_USERNAME", "irfan@nextgeni.com");
+            $mail->Password = env("MAIL_PASSWORD", "ubekjdyngmimnwxr");
+            $mail->setFrom(env("MAIL_USERNAME", "irfan@nextgeni.com"));
+            $mail->Subject = "Password Reset Link";
+            $mail->MsgHTML("Your password reset link is <a href='".$url."'>".$url."<a/>");
+            $mail->addAddress($user->email, "Recipient Name");
+            $mail->send();
+        } catch (phpmailerException $e) {
+            dd($e);
+        } catch (Exception $e) {
+            dd($e);
+        }
+    }
 }
